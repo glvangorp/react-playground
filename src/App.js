93bf12bf9c6222da68a78d2f1f571c7c25f13fbe
Greg Van Gorp <glvangorp@gmail.com>
2023-08-25 22:59:49 -0400

    Episode 3

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import pokemon from './pokemon.json';
+
+function App() {
+  return (
+    <div
+      style={{
+        margin: 'auto',
+        width: 800,
+        paddingTop: '1rem',
+      }}
+    >
+      <h1 className='title'>Pokemon Search</h1>
+      <table width='100%'>
+        <thead>
+          <tr>
+            <th>Pokemon</th>
+            <th>Type(s)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon.slice(0, 20).map((p) => (
+            <tr key={p.id}>
+              <td>{p.name.english}</td>
+              <td>{p.type.join(', ')}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
